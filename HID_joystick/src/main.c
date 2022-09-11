824be37f32846eca7f7248c7836a77685164c386
Embla Flatlandsmo <embla.flatlandsmo@gmail.com>
2022-09-11 16:03:38 +0200

    HID: single joystick functionality

--- HID_joystick/src/main.c
+++ HID_joystick/src/main.c
@@ -36,25 +36,13 @@
 /* Number of pixels by which the cursor is moved when a button is pushed. */
 #define MOVEMENT_SPEED              5
 /* Number of input reports in this application. */
-#define INPUT_REPORT_COUNT          3
-/* Length of Mouse Input Report containing button data. */
-#define INPUT_REP_BUTTONS_LEN       3
-/* Length of Mouse Input Report containing movement data. */
-#define INPUT_REP_MOVEMENT_LEN      3
-/* Length of Mouse Input Report containing media player data. */
-#define INPUT_REP_MEDIA_PLAYER_LEN  1
-/* Index of Mouse Input Report containing button data. */
-#define INPUT_REP_BUTTONS_INDEX     0
-/* Index of Mouse Input Report containing movement data. */
-#define INPUT_REP_MOVEMENT_INDEX    1
-/* Index of Mouse Input Report containing media player data. */
-#define INPUT_REP_MPLAYER_INDEX     2
-/* Id of reference to Mouse Input Report containing button data. */
-#define INPUT_REP_REF_BUTTONS_ID    1
-/* Id of reference to Mouse Input Report containing movement data. */
-#define INPUT_REP_REF_MOVEMENT_ID   2
-/* Id of reference to Mouse Input Report containing media player data. */
-#define INPUT_REP_REF_MPLAYER_ID    3
+#define INPUT_REPORT_COUNT          1
+/* Length of Game Pad Input Report containing movement data. */
+#define INPUT_REP_MOVEMENT_LEN      2
+/* Index of Game pad Input Report containing movement data. */
+#define INPUT_REP_MOVEMENT_INDEX    0
+/* Id of reference to Game pad Input Report containing movement data. */
+#define INPUT_REP_REF_MOVEMENT_ID   1
 
 /* HIDs queue size. */
 #define HIDS_QUEUE_SIZE 10
@@ -74,19 +62,20 @@
 
 /* HIDS instance. */
 BT_HIDS_DEF(hids_obj,
-	    INPUT_REP_BUTTONS_LEN,
-	    INPUT_REP_MOVEMENT_LEN,
-	    INPUT_REP_MEDIA_PLAYER_LEN);
+	    INPUT_REP_MOVEMENT_LEN);
 
 static struct k_work hids_work;
-struct mouse_pos {
+
+struct joystick_pos {
 	int16_t x_val;
 	int16_t y_val;
+	// int16_t right_x_val;
+	// int16_t right_y_val;
 };
 
 /* Mouse movement queue. */
 K_MSGQ_DEFINE(hids_queue,
-	      sizeof(struct mouse_pos),
+	      sizeof(struct joystick_pos),
 	      HIDS_QUEUE_SIZE,
 	      4);
 
@@ -375,125 +364,6 @@ static void hids_pm_evt_handler(enum bt_hids_pm_evt evt,
 	}
 }
 
-
-// static void hid_init(void)
-// {
-// 	int err;
-// 	struct bt_hids_init_param hids_init_param = { 0 };
-// 	struct bt_hids_inp_rep *hids_inp_rep;
-// 	static const uint8_t mouse_movement_mask[ceiling_fraction(INPUT_REP_MOVEMENT_LEN, 8)] = {0};
-
-// 	static const uint8_t report_map[] = {
-// 		0x05, 0x01,     /* Usage Page (Generic Desktop) */
-// 		0x09, 0x02,     /* Usage (Mouse) */
-
-// 		0xA1, 0x01,     /* Collection (Application) */
-
-// 		/* Report ID 1: Mouse buttons + scroll/pan */
-// 		0x85, 0x01,       /* Report Id 1 */
-// 		0x09, 0x01,       /* Usage (Pointer) */
-// 		0xA1, 0x00,       /* Collection (Physical) */
-// 		0x95, 0x05,       /* Report Count (3) */
-// 		0x75, 0x01,       /* Report Size (1) */
-// 		0x05, 0x09,       /* Usage Page (Buttons) */
-// 		0x19, 0x01,       /* Usage Minimum (01) */
-// 		0x29, 0x05,       /* Usage Maximum (05) */
-// 		0x15, 0x00,       /* Logical Minimum (0) */
-// 		0x25, 0x01,       /* Logical Maximum (1) */
-// 		0x81, 0x02,       /* Input (Data, Variable, Absolute) */
-// 		0x95, 0x01,       /* Report Count (1) */
-// 		0x75, 0x03,       /* Report Size (3) */
-// 		0x81, 0x01,       /* Input (Constant) for padding */
-// 		0x75, 0x08,       /* Report Size (8) */
-// 		0x95, 0x01,       /* Report Count (1) */
-// 		0x05, 0x01,       /* Usage Page (Generic Desktop) */
-// 		0x09, 0x38,       /* Usage (Wheel) */
-// 		0x15, 0x81,       /* Logical Minimum (-127) */
-// 		0x25, 0x7F,       /* Logical Maximum (127) */
-// 		0x81, 0x06,       /* Input (Data, Variable, Relative) */
-// 		0x05, 0x0C,       /* Usage Page (Consumer) */
-// 		0x0A, 0x38, 0x02, /* Usage (AC Pan) */
-// 		0x95, 0x01,       /* Report Count (1) */
-// 		0x81, 0x06,       /* Input (Data,Value,Relative,Bit Field) */
-// 		0xC0,             /* End Collection (Physical) */
-
-// 		/* Report ID 2: Mouse motion */
-// 		0x85, 0x02,       /* Report Id 2 */
-// 		0x09, 0x01,       /* Usage (Pointer) */
-// 		0xA1, 0x00,       /* Collection (Physical) */
-// 		0x75, 0x0C,       /* Report Size (12) */
-// 		0x95, 0x02,       /* Report Count (2) */
-// 		0x05, 0x01,       /* Usage Page (Generic Desktop) */
-// 		0x09, 0x30,       /* Usage (X) */
-// 		0x09, 0x31,       /* Usage (Y) */
-// 		0x16, 0x01, 0xF8, /* Logical maximum (2047) */
-// 		0x26, 0xFF, 0x07, /* Logical minimum (-2047) */
-// 		0x81, 0x06,       /* Input (Data, Variable, Relative) */
-// 		0xC0,             /* End Collection (Physical) */
-// 		0xC0,             /* End Collection (Application) */
-
-// 		/* Report ID 3: Advanced buttons */
-// 		0x05, 0x0C,       /* Usage Page (Consumer) */
-// 		0x09, 0x01,       /* Usage (Consumer Control) */
-// 		0xA1, 0x01,       /* Collection (Application) */
-// 		0x85, 0x03,       /* Report Id (3) */
-// 		0x15, 0x00,       /* Logical minimum (0) */
-// 		0x25, 0x01,       /* Logical maximum (1) */
-// 		0x75, 0x01,       /* Report Size (1) */
-// 		0x95, 0x01,       /* Report Count (1) */
-
-// 		0x09, 0xCD,       /* Usage (Play/Pause) */
-// 		0x81, 0x06,       /* Input (Data,Value,Relative,Bit Field) */
-// 		0x0A, 0x83, 0x01, /* Usage (Consumer Control Configuration) */
-// 		0x81, 0x06,       /* Input (Data,Value,Relative,Bit Field) */
-// 		0x09, 0xB5,       /* Usage (Scan Next Track) */
-// 		0x81, 0x06,       /* Input (Data,Value,Relative,Bit Field) */
-// 		0x09, 0xB6,       /* Usage (Scan Previous Track) */
-// 		0x81, 0x06,       /* Input (Data,Value,Relative,Bit Field) */
-
-// 		0x09, 0xEA,       /* Usage (Volume Down) */
-// 		0x81, 0x06,       /* Input (Data,Value,Relative,Bit Field) */
-// 		0x09, 0xE9,       /* Usage (Volume Up) */
-// 		0x81, 0x06,       /* Input (Data,Value,Relative,Bit Field) */
-// 		0x0A, 0x25, 0x02, /* Usage (AC Forward) */
-// 		0x81, 0x06,       /* Input (Data,Value,Relative,Bit Field) */
-// 		0x0A, 0x24, 0x02, /* Usage (AC Back) */
-// 		0x81, 0x06,       /* Input (Data,Value,Relative,Bit Field) */
-// 		0xC0              /* End Collection */
-// 	};
-
-// 	hids_init_param.rep_map.data = report_map;
-// 	hids_init_param.rep_map.size = sizeof(report_map);
-
-// 	hids_init_param.info.bcd_hid = BASE_USB_HID_SPEC_VERSION;
-// 	hids_init_param.info.b_country_code = 0x00;
-// 	hids_init_param.info.flags = (BT_HIDS_REMOTE_WAKE |
-// 				      BT_HIDS_NORMALLY_CONNECTABLE);
-
-// 	hids_inp_rep = &hids_init_param.inp_rep_group_init.reports[0];
-// 	hids_inp_rep->size = INPUT_REP_BUTTONS_LEN;
-// 	hids_inp_rep->id = INPUT_REP_REF_BUTTONS_ID;
-// 	hids_init_param.inp_rep_group_init.cnt++;
-
-// 	hids_inp_rep++;
-// 	hids_inp_rep->size = INPUT_REP_MOVEMENT_LEN;
-// 	hids_inp_rep->id = INPUT_REP_REF_MOVEMENT_ID;
-// 	hids_inp_rep->rep_mask = mouse_movement_mask;
-// 	hids_init_param.inp_rep_group_init.cnt++;
-
-// 	hids_inp_rep++;
-// 	hids_inp_rep->size = INPUT_REP_MEDIA_PLAYER_LEN;
-// 	hids_inp_rep->id = INPUT_REP_REF_MPLAYER_ID;
-// 	hids_init_param.inp_rep_group_init.cnt++;
-
-// 	hids_init_param.is_mouse = true;
-// 	hids_init_param.pm_evt_handler = hids_pm_evt_handler;
-
-// 	err = bt_hids_init(&hids_obj, &hids_init_param);
-// 	__ASSERT(err == 0, "HIDS initialization failed\n");
-// }
-
-
 static void hid_init(void)
 {
 	int err;
@@ -501,104 +371,25 @@ static void hid_init(void)
 	struct bt_hids_inp_rep *hids_inp_rep;
 	static const uint8_t mouse_movement_mask[ceiling_fraction(INPUT_REP_MOVEMENT_LEN, 8)] = {0};
 
-	// static const uint8_t report_map[] = {
-	// 	0x05, 0x01,     /* Usage Page (Generic Desktop) */
-	// 	0x09, 0x05,     /* Usage (Game Pad) */
-
-	// 	0xA1, 0x01,     /* Collection (Application) */
-	// 	0x85, 0x01,       /* Report Id 1 */
-	// 	0x05, 0x01,       /* Usage Page (Generic Desktop) */
-	// 	0x15, 0x81,  	  /* Logical Minimum (-127) */
-	// 	0x25, 0x7F,  	  /* Logical Maximum (127) */
-	// 	0x09, 0x30,  	  /* Usage (X) */
-	// 	0x09, 0x31,  	  /* Usage (Y) */
-	// 	0x09, 0x32,  	  /* Usage (Z) */
-	// 	0x09, 0x35,      /* Usage (Rz) */
-	// 	0x75, 0x08,      /* Report Size (8) */
-	// 	0x95, 0x04,      /* Report Count (4) */
-	// 	0x81, 0x02,      /* Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position) */
-	// 	0xC0,             /* End Collection (Application) */
-	// 	0xC0,        /* End Collection */
-	// };
-
-	
 	static const uint8_t report_map[] = {
 		0x05, 0x01,     /* Usage Page (Generic Desktop) */
-		0x09, 0x02,     /* Usage (Mouse) */
+		0x09, 0x05,     /* Usage (Game Pad) */
 
 		0xA1, 0x01,     /* Collection (Application) */
-
-		/* Report ID 1: Mouse buttons + scroll/pan */
+		0xa1, 0x00,       /* Collection (Physical) */
 		0x85, 0x01,       /* Report Id 1 */
-		0x09, 0x01,       /* Usage (Pointer) */
-		0xA1, 0x00,       /* Collection (Physical) */
-		0x95, 0x05,       /* Report Count (3) */
-		0x75, 0x01,       /* Report Size (1) */
-		0x05, 0x09,       /* Usage Page (Buttons) */
-		0x19, 0x01,       /* Usage Minimum (01) */
-		0x29, 0x05,       /* Usage Maximum (05) */
-		0x15, 0x00,       /* Logical Minimum (0) */
-		0x25, 0x01,       /* Logical Maximum (1) */
-		0x81, 0x02,       /* Input (Data, Variable, Absolute) */
-		0x95, 0x01,       /* Report Count (1) */
-		0x75, 0x03,       /* Report Size (3) */
-		0x81, 0x01,       /* Input (Constant) for padding */
-		0x75, 0x08,       /* Report Size (8) */
-		0x95, 0x01,       /* Report Count (1) */
 		0x05, 0x01,       /* Usage Page (Generic Desktop) */
-		0x09, 0x38,       /* Usage (Wheel) */
-		0x15, 0x81,       /* Logical Minimum (-127) */
-		0x25, 0x7F,       /* Logical Maximum (127) */
-		0x81, 0x06,       /* Input (Data, Variable, Relative) */
-		0x05, 0x0C,       /* Usage Page (Consumer) */
-		0x0A, 0x38, 0x02, /* Usage (AC Pan) */
-		0x95, 0x01,       /* Report Count (1) */
-		0x81, 0x06,       /* Input (Data,Value,Relative,Bit Field) */
-		0xC0,             /* End Collection (Physical) */
-
-		/* Report ID 2: Mouse motion */
-		0x85, 0x02,       /* Report Id 2 */
-		0x09, 0x01,       /* Usage (Pointer) */
-		0xA1, 0x00,       /* Collection (Physical) */
-		0x75, 0x0C,       /* Report Size (12) */
-		0x95, 0x02,       /* Report Count (2) */
-		0x05, 0x01,       /* Usage Page (Generic Desktop) */
-		0x09, 0x30,       /* Usage (X) */
-		0x09, 0x31,       /* Usage (Y) */
-		0x16, 0x01, 0xF8, /* Logical maximum (2047) */
-		0x26, 0xFF, 0x07, /* Logical minimum (-2047) */
-		0x81, 0x06,       /* Input (Data, Variable, Relative) */
-		0xC0,             /* End Collection (Physical) */
-		0xC0,             /* End Collection (Application) */
-
-		/* Report ID 3: Advanced buttons */
-		0x05, 0x0C,       /* Usage Page (Consumer) */
-		0x09, 0x01,       /* Usage (Consumer Control) */
-		0xA1, 0x01,       /* Collection (Application) */
-		0x85, 0x03,       /* Report Id (3) */
-		0x15, 0x00,       /* Logical minimum (0) */
-		0x25, 0x01,       /* Logical maximum (1) */
-		0x75, 0x01,       /* Report Size (1) */
-		0x95, 0x01,       /* Report Count (1) */
-
-		0x09, 0xCD,       /* Usage (Play/Pause) */
-		0x81, 0x06,       /* Input (Data,Value,Relative,Bit Field) */
-		0x0A, 0x83, 0x01, /* Usage (Consumer Control Configuration) */
-		0x81, 0x06,       /* Input (Data,Value,Relative,Bit Field) */
-		0x09, 0xB5,       /* Usage (Scan Next Track) */
-		0x81, 0x06,       /* Input (Data,Value,Relative,Bit Field) */
-		0x09, 0xB6,       /* Usage (Scan Previous Track) */
-		0x81, 0x06,       /* Input (Data,Value,Relative,Bit Field) */
-
-		0x09, 0xEA,       /* Usage (Volume Down) */
-		0x81, 0x06,       /* Input (Data,Value,Relative,Bit Field) */
-		0x09, 0xE9,       /* Usage (Volume Up) */
-		0x81, 0x06,       /* Input (Data,Value,Relative,Bit Field) */
-		0x0A, 0x25, 0x02, /* Usage (AC Forward) */
-		0x81, 0x06,       /* Input (Data,Value,Relative,Bit Field) */
-		0x0A, 0x24, 0x02, /* Usage (AC Back) */
-		0x81, 0x06,       /* Input (Data,Value,Relative,Bit Field) */
-		0xC0              /* End Collection */
+		0x15, 0x81,  	  /* Logical Minimum (-127) */
+		0x25, 0x7F,  	  /* Logical Maximum (127) */
+		0x09, 0x30,  	  /* Usage (X) */
+		0x09, 0x31,  	  /* Usage (Y) */
+		// 0x09, 0x32,  	  /* Usage (Z) */
+		// 0x09, 0x35,      /* Usage (Rx) */
+		0x75, 0x08,      /* Report Size (8) */
+		0x95, 0x02,      /* Report Count (2) */
+		0x81, 0x02,      /* Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position) */
+		0xC0,            /* End Collection (Physical) */
+		0xC0,        /* End Collection (Application) */
 	};
 
 	hids_init_param.rep_map.data = report_map;
@@ -610,83 +401,68 @@ static void hid_init(void)
 				      BT_HIDS_NORMALLY_CONNECTABLE);
 
 	hids_inp_rep = &hids_init_param.inp_rep_group_init.reports[0];
-	hids_inp_rep->size = INPUT_REP_BUTTONS_LEN;
-	hids_inp_rep->id = INPUT_REP_REF_BUTTONS_ID;
-	hids_init_param.inp_rep_group_init.cnt++;
-
-	hids_inp_rep++;
 	hids_inp_rep->size = INPUT_REP_MOVEMENT_LEN;
 	hids_inp_rep->id = INPUT_REP_REF_MOVEMENT_ID;
-	hids_inp_rep->rep_mask = mouse_movement_mask;
+	// hids_inp_rep->rep_mask = mouse_movement_mask;
+	hids_inp_rep->rep_mask = NULL;
 	hids_init_param.inp_rep_group_init.cnt++;
-
-	hids_inp_rep++;
-	hids_inp_rep->size = INPUT_REP_MEDIA_PLAYER_LEN;
-	hids_inp_rep->id = INPUT_REP_REF_MPLAYER_ID;
-	hids_init_param.inp_rep_group_init.cnt++;
-
-	hids_init_param.is_mouse = true;
 	hids_init_param.pm_evt_handler = hids_pm_evt_handler;
 
 	err = bt_hids_init(&hids_obj, &hids_init_param);
 	__ASSERT(err == 0, "HIDS initialization failed\n");
 }
 
-
-
-static void mouse_movement_send(int16_t x_delta, int16_t y_delta)
+static void gamepad_movement_send(uint8_t val)
 {
 	for (size_t i = 0; i < CONFIG_BT_HIDS_MAX_CLIENT_COUNT; i++) {
 
 		if (!conn_mode[i].conn) {
 			continue;
 		}
+		uint8_t buffer[2];
 
-		if (conn_mode[i].in_boot_mode) {
-			x_delta = MAX(MIN(x_delta, SCHAR_MAX), SCHAR_MIN);
-			y_delta = MAX(MIN(y_delta, SCHAR_MAX), SCHAR_MIN);
+		// uint8_t x_buff[2];
+		// uint8_t y_buff[2];
 
-			bt_hids_boot_mouse_inp_rep_send(&hids_obj,
-							     conn_mode[i].conn,
-							     NULL,
-							     (int8_t) x_delta,
-							     (int8_t) y_delta,
-							     NULL);
-		} else {
-			uint8_t x_buff[2];
-			uint8_t y_buff[2];
-			uint8_t buffer[INPUT_REP_MOVEMENT_LEN];
+		// int16_t x = MAX(MIN(x_delta, 0x07ff), -0x07ff);
+		// int16_t y = MAX(MIN(y_delta, 0x07ff), -0x07ff);
 
-			int16_t x = MAX(MIN(x_delta, 0x07ff), -0x07ff);
-			int16_t y = MAX(MIN(y_delta, 0x07ff), -0x07ff);
+		// /* Convert to little-endian. */
+		// sys_put_le16(x, x_buff);
+		// sys_put_le16(y, y_buff);
 
-			/* Convert to little-endian. */
-			sys_put_le16(x, x_buff);
-			sys_put_le16(y, y_buff);
+		// /* Encode report. */
+		// // BUILD_ASSERT(sizeof(buffer) == 3,
+		// // 			"Only 2 axis, 12-bit each, are supported");
+		// BUILD_ASSERT(sizeof(buffer) == 2,
+		// 			"Only 1 joystick, 2 axis with 8bit each is supported");
+		
+		// buffer[0] = x_buff[0];
+		// buffer[1] = (y_buff[0] << 4) | (x_buff[1] & 0x0f);
+		// buffer[2] = (y_buff[1] << 4) | (y_buff[0] >> 4);
+		buffer[0] = val;
+		buffer[1] = val;
+		printk("Sending movement: x=%d, y=%d", val, val);
+		
 
-			/* Encode report. */
-			BUILD_ASSERT(sizeof(buffer) == 3,
-					 "Only 2 axis, 12-bit each, are supported");
-
-			buffer[0] = x_buff[0];
-			buffer[1] = (y_buff[0] << 4) | (x_buff[1] & 0x0f);
-			buffer[2] = (y_buff[1] << 4) | (y_buff[0] >> 4);
-
-
-			bt_hids_inp_rep_send(&hids_obj, conn_mode[i].conn,
-						  INPUT_REP_MOVEMENT_INDEX,
-						  buffer, sizeof(buffer), NULL);
-		}
+		bt_hids_inp_rep_send(&hids_obj, conn_mode[i].conn,
+						INPUT_REP_MOVEMENT_INDEX,
+						buffer, sizeof(buffer), NULL);
 	}
 }
 
-
 static void mouse_handler(struct k_work *work)
 {
-	struct mouse_pos pos;
-
+	struct joystick_pos pos;
+	static uint8_t val = 0x00;
 	while (!k_msgq_get(&hids_queue, &pos, K_NO_WAIT)) {
-		mouse_movement_send(pos.x_val, pos.y_val);
+		if (val)
+		{
+			val = 0;
+		} else {
+			val = UINT8_MAX;
+		}
+		gamepad_movement_send(val);
 	}
 }
 
@@ -812,10 +588,10 @@ static void num_comp_reply(bool accept)
 void button_changed(uint32_t button_state, uint32_t has_changed)
 {
 	bool data_to_send = false;
-	struct mouse_pos pos;
+	struct joystick_pos pos;
 	uint32_t buttons = button_state & has_changed;
 
-	memset(&pos, 0, sizeof(struct mouse_pos));
+	memset(&pos, 0, sizeof(struct joystick_pos));
 
 	if (k_msgq_num_used_get(&mitm_queue)) {
 		if (buttons & KEY_PAIRING_ACCEPT) {
