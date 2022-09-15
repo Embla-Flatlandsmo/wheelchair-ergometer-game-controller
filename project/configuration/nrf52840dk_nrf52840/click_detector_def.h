565ef6c35a535721d18121490d37bd9eaa88fba9
Embla Flatlandsmo <embla.flatlandsmo@nordicsemi.no>
2022-09-15 11:26:02 +0200

    Add CAF functionality

--- project/configuration/nrf52840dk_nrf52840/click_detector_def.h
+++ project/configuration/nrf52840dk_nrf52840/click_detector_def.h
@@ -5,7 +5,7 @@
  */
 
 #include <caf/click_detector.h>
-
+#include <caf/key_id.h>
 /* This configuration file is included only once from click_detector module
  * and holds information about click detector configuration.
  */
@@ -13,11 +13,16 @@
 /* This structure enforces the header file is included only once in the build.
  * Violating this requirement triggers a multiple definition error at link time.
  */
-const struct {} click_detector_def_include_once;
+const struct
+{
+} click_detector_def_include_once;
 
 static const struct click_detector_config click_detector_config[] = {
 	{
-		.key_id = CONFIG_DESKTOP_BLE_PEER_CONTROL_BUTTON,
-		.consume_button_event = false,
+		.key_id = KEY_ID(0x00, 0x00),
+		.consume_button_event = true,
 	},
-};
+	{
+		.key_id = KEY_ID(0x00, 0x01),
+		.consume_button_event = true,
+	}};
--- project/src/main.c
+++ project/src/main.c
@@ -5,10 +5,10 @@
  */
 
 #include <app_event_manager.h>
-// #include <config_event.h>
 #include <zephyr/logging/log.h>
 
 #define MODULE main
+#include <caf/events/module_state_event.h>
 
 LOG_MODULE_REGISTER(MODULE);
 
@@ -17,6 +17,7 @@ void main(void)
 	if (app_event_manager_init()) {
 		LOG_ERR("Application Event Manager not initialized");
 	} else {
+		module_set_state(MODULE_STATE_READY);
 		// struct config_event *event = new_config_event();
 
 		// event->init_value1 = INIT_VALUE1;
